6eb608eacd292a7a6ffcd5cb85eeeb193753ddf6
Nikita Alpatov <nikitaa95@mail.ru>
2025-02-23 13:46:07 +0200

    fix: adjust content obj for comment

--- dist/routers/comments/comments.router.js
+++ dist/routers/comments/comments.router.js
@@ -7,9 +7,8 @@ exports.commentsRouter = void 0;
 const express_1 = require("express");
 const posts_middlewares_1 = require("../posts/posts.middlewares");
 const comments_controller_1 = __importDefault(require("./comments.controller"));
+const bearerAuthorizationValidator_1 = require("../../authorization/middlewares/bearerAuthorizationValidator");
 exports.commentsRouter = (0, express_1.Router)({ mergeParams: true });
 exports.commentsRouter.get("/:id", comments_controller_1.default.getCommentById);
-exports.commentsRouter.put("/:id", 
-// bearerAuthorizationValidator,
-posts_middlewares_1.postCommentContentValidator, comments_controller_1.default.updateCommentForPost);
+exports.commentsRouter.put("/:id", bearerAuthorizationValidator_1.bearerAuthorizationValidator, posts_middlewares_1.postCommentContentValidator, comments_controller_1.default.updateCommentForPost);
 exports.commentsRouter.delete("/:id", comments_controller_1.default.deleteCommentById);
