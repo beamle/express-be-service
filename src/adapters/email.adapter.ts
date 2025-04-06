8c7ce235c1424da4c1c65d4e2262f3e52dd0e56b
Nikita Alpatov <nikitaa95@mail.ru>
2025-04-06 16:59:41 +0300

    refactor: add error cathc for email sending

--- src/adapters/email.adapter.ts
+++ src/adapters/email.adapter.ts
@@ -7,23 +7,25 @@ class EmailAdapter {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // true for port 465, false for other ports
+      // port: 587,
+      // secure: false, // true for port 465, false for other ports
       auth: {
-        user: "nikitadevmode@gmail.com",
-        pass: "tvbt ipsb dzjh kgud",
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
       },
     });
 
-    const info = await transporter.sendMail({
-      from: 'Nikita', // sender address
-      to: email, // list of receivers
-      subject: subject, // Subject line
-      // text: req.body.text, // plain text body
-      html: message, // html body
-    });
-
-    return info
+    try {
+      const info = await transporter.sendMail({
+        from: 'Nikita', // sender address
+        to: email, // list of receivers
+        subject: subject, // Subject line
+        html: message, // html body
+      });
+      return info
+    } catch (e) {
+      console.log('Error sending email: ', e)
+    }
   }
 }
 
