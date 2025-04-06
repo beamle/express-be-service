36a618e037daf29543331f13df9c4af5d3acfe6b
Nikita Alpatov <nikitaa95@mail.ru>
2025-04-06 16:13:03 +0300

    wtf

--- dist/routers/auth/auth.router.js
+++ dist/routers/auth/auth.router.js
@@ -13,5 +13,5 @@ exports.authRouter = (0, express_1.Router)({});
 exports.authRouter.post("/login", auth_controller_1.default.login);
 exports.authRouter.post("/registration", ...auth_middlewares_1.authValidators, validationHelpers_1.inputCheckErrorsFormatter, auth_controller_1.default.registration);
 exports.authRouter.post("/registration-confirmation", auth_controller_1.default.confirmEmail);
-exports.authRouter.post("/registration-email-resending", auth_controller_1.default.resendEmail);
+exports.authRouter.get("/registration-email-resending", auth_controller_1.default.resendEmail);
 exports.authRouter.get("/me", bearerAuthorizationValidator_1.bearerAuthorizationValidator, auth_controller_1.default.me);
